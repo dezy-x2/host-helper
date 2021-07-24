017b3a089881d4a2e76332aa90dc2345b4970f4a
Daniel Desmond <df.dezmond9@gmail.com>
2021-07-24 10:34:18 -0400

    moved some of the logic around

--- Restraunt.java
+++ Restraunt.java
@@ -3,11 +3,32 @@ import java.util.ArrayList;
 public class Restraunt {
     public int numTables;
     private ArrayList<TableGroup> tableGroups;
+    private ArrayList<Table> tables;
+    private ArrayList<Server> servers;
 
     Restraunt(int numTables) {
         this.numTables = numTables;
         this.tableGroups = new ArrayList<TableGroup>();
+        this.tables = new ArrayList<Table>();
+        this.servers = new ArrayList<Server>();
     }
+
+    private void generateTableGroups() {
+        System.out.println("Generating table groups...");
+    }
+
+    public void addTables(Table... tables) {
+        for (Table table : tables) {
+            this.tables.add(table);
+        }
+    }
+
+    public void addServer(Server... servers) {
+        for (Server server : servers) {
+            this.servers.add(server);
+        }
+    }
+
     public static void main(String[] args) {
 
     }
--- TableGroup.java
+++ TableGroup.java
@@ -2,22 +2,17 @@ import java.util.ArrayList;
 public class TableGroup {
     public int id;
     private ArrayList<Table> tables;
-    private ArrayList<Server> servers;
+    private Server server;
 
-    public TableGroup(int id) {
+    public TableGroup(int id, Server server) {
         this.id = id;
         this.tables = new ArrayList<Table>();
-        this.servers = new ArrayList<Server>();
+        this.server = server;
     }
 
-    private void addTables(Table... tables) {
+    public void addTables(Table... tables) {
         for (Table table : tables) {
             this.tables.add(table);
-        }
-    }
-    private void addServers(Server... servers) {
-        for (Server server : servers) {
-            this.servers.add(server);
         }
     }
 
