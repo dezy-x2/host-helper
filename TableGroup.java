666579499ee42c9324681a928604e865fd17e345
Daniel Desmond <df.dezmond9@gmail.com>
2021-07-24 18:37:34 -0400

    rewrote TableGroup

--- TableGroup.java
+++ TableGroup.java
@@ -1,26 +1,34 @@
 import java.util.ArrayList;
 public class TableGroup {
-    public int id;
     private ArrayList<Table> tables;
     private Server server;
 
-    public TableGroup(int id, Server server) {
-        this.id = id;
-        this.tables = new ArrayList<Table>();
+    public TableGroup(Server server, ArrayList<Table> tables) {
+        this.tables = tables;
         this.server = server;
     }
 
-    public void addTables(Table... tables) {
+    public boolean isFull() {
         for (Table table : tables) {
-            this.tables.add(table);
+            if (table.isAvaliable()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void fillTable(boolean reserved) {
+        if(reserved) {
+            System.out.println("Reserving table...");
+        } else {
+            System.out.println("Filling table...");
         }
     }
 
     @Override
     public String toString() {
         return "TableGroup{" +
-                "id=" + id +
-                ", tables=" + tables +
+                "tables=" + tables +
                 ", server=" + server +
                 '}';
     }
