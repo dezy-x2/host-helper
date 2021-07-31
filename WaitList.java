7bae9376b9bb1df708b5576d078fb4d24693a4a4
Daniel Desmond <df.dezmond9@gmail.com>
2021-07-30 20:52:49 -0400

    made method to see if there are eligible patrons

--- WaitList.java
+++ WaitList.java
@@ -11,4 +11,13 @@ public class WaitList {
         this.waitList.add(patron);
         System.out.println(patron.name + " has been added to the wait list.");
     }
+
+    public boolean eligiblePatron(Table table) {
+        for (Patron patron : waitList) {
+            if (table.numSeats >= patron.groupCount) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
