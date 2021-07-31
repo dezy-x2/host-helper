620cdcbd40ce466865a6341506813221df4acdb8
Daniel Desmond <df.dezmond9@gmail.com>
2021-07-30 20:29:16 -0400

    created Patron constructor

--- Patron.java
+++ Patron.java
@@ -1,5 +1,14 @@
 import java.util.ArrayList;
 
 public class Patron {
+    public String name;
+    private int phoneNumber;
+    public int groupCount;
+
+    public Patron(String name, int phoneNumber, int groupCount) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.groupCount = groupCount;
+    }
 
 }
