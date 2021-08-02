1084af008e23b2b9ab638059e18a4852791c0e58
Daniel Desmond <df.dezmond9@gmail.com>
2021-08-01 21:47:18 -0400

    added func to find the table with the lowest wait

--- WaitList.java
+++ WaitList.java
@@ -22,6 +22,25 @@ public class WaitList {
         return false;
     }
 
+    public Table eligibleTable(int groupCount, ArrayList<Table> tables) {
+        ArrayList<Table> tablesWithSeats = new ArrayList<Table>();
+        Table bestTime = null;
+        long max = Integer.MAX_VALUE;
+        for (Table table : tables) {
+            if (table.numSeats >= groupCount) {
+                tablesWithSeats.add(table);
+            }
+        }
+
+        for (Table table : tablesWithSeats) {
+            if (table.seatingTimePassed() < max) {
+                max = table.seatingTimePassed();
+                bestTime = table;
+            }
+        }
+        return bestTime;
+    }
+
     public Patron seatFromWaitList(Table table) {
         for (int i=0; i<waitList.size(); i++) {
             if (table.numSeats >= waitList.get(i).groupCount) {
