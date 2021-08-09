91776df4afc6aba2989984a9e7fd0c11249f4077
Daniel Desmond <df.dezmond9@gmail.com>
2021-08-08 21:22:23 -0400

    adjusted maxEmptyTables to use ratio

--- Restaurant.java
+++ Restaurant.java
@@ -72,7 +72,7 @@ public class Restaurant {
     private int maxEmptyTables(ArrayList<TableGroup> tableGroups) {
         int max = Integer.MAX_VALUE;
         for (TableGroup tableGroup : tableGroups) {
-            int emptyTables = tableGroup.emptyCount();
+            int emptyTables = (int) (tableGroup.getEmptyRatio() + 0.5);
             if (emptyTables < max) {
                 max = emptyTables;
             }
